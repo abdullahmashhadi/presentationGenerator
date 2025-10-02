71efafb44c6202581a8d90311fdd97138524e20f
abdullahmashhadi <abdullah6blue@gmail.com>
2025-10-02 09:47:10 +0500

    changed wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,8 +1,1 @@
-from app import app
-
-# Vercel serverless function handler
-def handler(request, response):
-    return app(request, response)
-
-if __name__ == '__main__':
-    app.run(debug=False)
+from app import app as application  # expose as "application"
